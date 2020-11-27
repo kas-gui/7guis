4778a4f1650e95a71615d3e073502e58255974b7
Diggory Hardy <git@dhardy.name>
2020-11-27 11:48:37 +0000

    Add patches to use KAS from git; add progress bar

--- src/timer.rs
+++ src/timer.rs
@@ -7,8 +7,13 @@
 
 use kas::event::VoidResponse;
 use kas::prelude::*;
-use kas::widget::{Label, Slider, TextButton, Window};
+use kas::widget::{Label, ProgressBar, Slider, TextButton, Window};
+use kas::Right;
 use std::time::{Duration, Instant};
+
+const DUR_MIN: Duration = Duration::from_secs(0);
+const DUR_MAX: Duration = Duration::from_secs(30);
+const DUR_STEP: Duration = Duration::from_millis(100);
 
 pub fn window() -> Box<dyn kas::Window> {
     Box::new(Window::new(
@@ -18,17 +23,17 @@ pub fn window() -> Box<dyn kas::Window> {
             #[layout(column)]
             #[handler(handle = noauto)]
             struct {
-                #[widget] _ = Label::new("Elapsed time: GAUGE"), // TODO: progress bar
+                #[widget] progress: ProgressBar<Right> = ProgressBar::new(),
                 #[widget] elapsed: Label<String> = Label::new("0.0s".to_string()),
                 #[widget(handler=slider)] _ = make_widget! {
                     // TODO: this layout widget is used only to add a label.
                     // Allow all controls to have labels without this?
                     #[layout(row)]
-                    #[handler(msg = u64)]
+                    #[handler(msg = Duration)]
                     struct {
                         #[widget] _ = Label::new("Duration:"),
-                        #[widget] _ = Slider::new_with_direction(0u32, 30_000, 100, kas::Right)
-                            .with_value(10_000),
+                        #[widget] _ = Slider::new_with_direction(DUR_MIN, DUR_MAX, DUR_STEP, Right)
+                            .with_value(Duration::from_secs(10)),
                     }
                 },
                 #[widget(handler=reset)] _ = TextButton::new("Reset", ()),
@@ -39,7 +44,7 @@ pub fn window() -> Box<dyn kas::Window> {
             impl WidgetConfig {
                 fn configure(&mut self, mgr: &mut Manager) {
                     self.start = Some(Instant::now());
-                    mgr.update_on_timer(Duration::from_millis(100), self.id());
+                    mgr.update_on_timer(DUR_STEP, self.id());
                 }
             }
             impl Handler {
@@ -51,12 +56,14 @@ pub fn window() -> Box<dyn kas::Window> {
                             if let Some(start) = self.start {
                                 let mut dur = self.saved + (Instant::now() - start);
                                 if dur < self.dur {
-                                    mgr.update_on_timer(Duration::from_millis(100), self.id());
+                                    mgr.update_on_timer(DUR_STEP, self.id());
                                 } else {
                                     dur = self.dur;
                                     self.saved = dur;
                                     self.start = None;
                                 }
+                                let frac = dur.as_secs_f32() / self.dur.as_secs_f32();
+                                *mgr += self.progress.set_value(frac);
                                 *mgr += self.elapsed.set_string(format!(
                                     "{}.{}s",
                                     dur.as_secs(),
@@ -70,24 +77,28 @@ pub fn window() -> Box<dyn kas::Window> {
                 }
             }
             impl {
-                fn slider(&mut self, mgr: &mut Manager, millis: u64) -> VoidResponse {
-                    self.dur = Duration::from_millis(millis);
+                fn slider(&mut self, mgr: &mut Manager, dur: Duration) -> VoidResponse {
+                    self.dur = dur;
+                    let mut elapsed = self.saved;
                     if let Some(start) = self.start {
-                        let elapsed = (Instant::now() - start) + self.saved;
+                        elapsed += Instant::now() - start;
                         if elapsed >= self.dur {
                             self.saved = elapsed;
                             self.start = None;
                         }
                     } else if self.saved < self.dur {
                         self.start = Some(Instant::now());
-                        mgr.update_on_timer(Duration::from_millis(100), self.id());
+                        mgr.update_on_timer(DUR_STEP, self.id());
                     }
+                    let frac = elapsed.as_secs_f32() / self.dur.as_secs_f32();
+                    *mgr += self.progress.set_value(frac);
                     Response::None
                 }
                 fn reset(&mut self, mgr: &mut Manager, _: ()) -> VoidResponse {
                     self.saved = Duration::default();
                     self.start = Some(Instant::now());
-                    mgr.update_on_timer(Duration::from_millis(100), self.id());
+                    mgr.update_on_timer(DUR_STEP, self.id());
+                    *mgr += self.progress.set_value(0.0);
                     *mgr += self.elapsed.set_string("0.0s".to_string());
                     Response::None
                 }
