957523d1e319189c8fd2441efe9200b6906ac053
Diggory Hardy <git@dhardy.name>
2022-07-20 12:07:38 +0100

    Update flight_booker

--- src/flight_booker.rs
+++ src/flight_booker.rs
@@ -7,13 +7,19 @@
 
 use chrono::{Duration, Local, NaiveDate};
 use kas::prelude::*;
-use kas::widgets::{ComboBox, EditBox, EditField, EditGuard, MessageBox, TextButton, Window};
+use kas::widgets::dialog::MessageBox;
+use kas::widgets::menu::MenuEntry;
+use kas::widgets::{ComboBox, EditBox, EditField, EditGuard, TextButton};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, VoidMsg)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Flight {
     OneWay,
     Return,
 }
+#[derive(Clone, Debug)]
+struct ActionDates;
+#[derive(Clone, Debug)]
+struct ActionBook;
 
 // TODO: consider adding a view-and-edit widget (like SingleView but supporting
 // text editing) so that string representation is just a view of date repr.
@@ -27,8 +33,7 @@ impl Guard {
     }
 }
 impl EditGuard for Guard {
-    type Msg = ();
-    fn edit(edit: &mut EditField<Self>, _: &mut Manager) -> Option<()> {
+    fn edit(edit: &mut EditField<Self>, mgr: &mut EventMgr) {
         let date = NaiveDate::parse_from_str(edit.get_str().trim(), "%Y-%m-%d");
         edit.guard.date = match date {
             Ok(date) => Some(date),
@@ -40,64 +45,72 @@ impl EditGuard for Guard {
         };
         edit.set_error_state(edit.guard.date.is_none());
 
-        // On any change, we notify the parent that it should call date():
-        Some(())
+        // On any change, we notify the parent that it should update the book button:
+        mgr.push_msg(ActionDates);
     }
 }
 
-pub fn window() -> Box<dyn kas::Window> {
+pub fn window() -> Box<dyn Window> {
     // Default dates:
     let out = Local::today().naive_local();
     let back = out + Duration::days(7);
 
     let d1 = EditBox::new(out.format("%Y-%m-%d").to_string()).with_guard(Guard::new(out));
-    let d2 = EditBox::new(back.format("%Y-%m-%d").to_string())
-        .with_guard(Guard::new(back))
-        .with_disabled(true);
+    let d2 = EditBox::new(back.format("%Y-%m-%d").to_string()).with_guard(Guard::new(back));
 
-    let combo_labels = ["One-way flight", "Return flight"];
-    let combo_values = [Flight::OneWay, Flight::Return];
-
-    Box::new(Window::new(
-        "Flight Booker",
-        make_widget! {
-            #[widget]
-            #[layout(column)]
-            #[handler(msg = VoidMsg)]
-            struct {
-                #[widget(use_msg = combo)] _: ComboBox<Flight> = ComboBox::new(&combo_labels, 0)
-                    .on_select(move |_, index| Some(combo_values[index])),
-                #[widget(use_msg = date)] d1: EditBox<Guard> = d1,
-                #[widget(use_msg = date)] d2: EditBox<Guard> = d2,
-                #[widget(use_msg = book)] book = TextButton::new_msg("Book", ()),
-            }
-            impl {
-                fn combo(&mut self, mgr: &mut Manager, msg: Flight) {
-                    *mgr |= self.d2.set_disabled(msg == Flight::OneWay);
-                    self.date(mgr, ());
-                }
-                fn date(&mut self, mgr: &mut Manager, _: ()) {
-                    let is_ready = match self.d1.guard.date {
-                        None => false,
-                        Some(_) if self.d2.is_disabled() => true,
-                        Some(d1) => {
-                            match self.d2.guard.date {
-                                None => false,
-                                Some(d2) => {
-                                    // TODO: display error in GUI
-                                    if !(d1 < d2) {
-                                        println!("Out-bound flight must be before return flight!");
-                                    }
-                                    d1 < d2
-                                }
+    Box::new(impl_singleton! {
+        #[derive(Debug)]
+        #[widget {
+            layout = column: [
+                self.combo,
+                self.d1,
+                self.d2,
+                self.book,
+            ];
+        }]
+        struct {
+            core: widget_core!(),
+            #[widget] combo: ComboBox<Flight> = ComboBox::new_vec(vec![
+                MenuEntry::new("One-way flight", Flight::OneWay),
+                MenuEntry::new("Return flight", Flight::Return),
+            ]),
+            #[widget] d1: EditBox<Guard> = d1,
+            #[widget] d2: EditBox<Guard> = d2,
+            #[widget] book = TextButton::new_msg("Book", ActionBook),
+        }
+        impl Self {
+            fn update_dates(&mut self, mgr: &mut EventMgr) {
+                let is_ready = match self.d1.guard.date.as_ref() {
+                    None => false,
+                    Some(_) if mgr.is_disabled(self.d2.id_ref()) => true,
+                    Some(d1) => {
+                        match self.d2.guard.date.as_ref() {
+                            None => false,
+                            Some(d2) if d1 < d2 => true,
+                            _ => {
+                                // TODO: display error in GUI
+                                println!("Out-bound flight must be before return flight!");
+                                false
                             }
                         }
-                    };
-                    *mgr |= self.book.set_disabled(!is_ready);
-                }
-                fn book(&mut self, mgr: &mut Manager, _: ()) {
+                    }
+                };
+                mgr.set_disabled(self.book.id(), !is_ready);
+            }
+        }
+        impl Widget for Self {
+            fn configure(&mut self, mgr: &mut ConfigMgr) {
+                mgr.set_disabled(self.d2.id(), true);
+            }
+            fn handle_message(&mut self, mgr: &mut EventMgr, _: usize) {
+                if let Some(flight) = mgr.try_pop_msg::<Flight>() {
+                    mgr.set_disabled(self.d2.id(), flight == Flight::OneWay);
+                    self.update_dates(mgr);
+                } else if let Some(ActionDates) = mgr.try_pop_msg() {
+                    self.update_dates(mgr);
+                } else if let Some(ActionBook) = mgr.try_pop_msg() {
                     let d1 = self.d1.guard.date.unwrap();
-                    let msg = if self.d2.is_disabled() {
+                    let msg = if mgr.is_disabled(self.d2.id_ref()) {
                         format!("You have booked a one-way flight on {}", d1.format("%Y-%m-%d"))
                     } else {
                         let d2 = self.d2.guard.date.unwrap();
@@ -110,6 +123,11 @@ pub fn window() -> Box<dyn kas::Window> {
                     mgr.add_window(Box::new(MessageBox::new("Booker result", msg)));
                 }
             }
-        },
-    ))
+        }
+        impl Window for Self {
+            fn title(&self) -> &str {
+                "Flight Booker"
+            }
+        }
+    })
 }
